4d40e7593875d93be81cb9ea4b067c653a07b14b
Medici <alessandro.accurso@epitech.eu>
2021-12-13 14:01:10 +0100

    Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: "%s",
-    title: "Old Rating Movies",
+    title: "Rotten Tomato",
     htmlAttrs: {
       lang: "en"
     },
